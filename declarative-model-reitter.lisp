fb2ee77c043c8166f7fb9b83939d7b92bed279ec
TheRealDrDre <stocco@uw.edu>
2019-04-18 18:36:03 -0700

    Better version of Reitter/Declarative model

--- declarative-model-reitter.lisp
+++ declarative-model-reitter.lisp
@@ -4,6 +4,30 @@
 ;;; Simulates SP as an effect of declarative memory usage. Follows
 ;;; the ideas of Reitter et al.
 ;;; -------------------------------------------------------------- ;;;
+;;; Notes:
+;;; The model should do something like
+;;; 1. Develop a semantic representation (agent verb object)
+;;; 2. Use a syntactic structure to produce a sentence noun1-verb-noun2
+;;;
+;;; * A sentence should have the following structure noun1-verb-noun2 (voice)
+;;; * When struct is active, the sentence is created as agent-verb-object-actuve
+;;; * When struct is passive, the sentence is created as oject-verb-agent-passive.
+;;;
+;;; Similarly, when reading a sentence, the sentence is read as noun1-verb-noun2.
+;;; Then, a structure is retrieved depending on the 'voice' marker.
+;;; If structure = active, the interpretation is produced as
+;;;
+;;;    agent/noun1 - verb - object/noun2
+;;;
+;;; If structure = passive, the interpretation is produced as
+;;;
+;;;    agent/noun2 - verb - object/noun1
+;;;
+;;;
+;;; So the process is picture -> semantics -> sentence for production,
+;;; and sentence -> semantics -> picture for verification.
+;;; Problem: How do handle semantics and sentence in two different buffers?  
+
 
 (clear-all)
 
@@ -14,9 +38,9 @@
   
 (chunk-type sentence
             string
-            agent
-            object
+            noun1
             verb
+            noun2
             voice
             syntax-correct
             semantics-correct)
@@ -27,12 +51,22 @@
             name)
   
 (chunk-type picture
+            kind
+            agent
+            object
+            verb)
+
+(chunk-type semantics
             agent
             object
             verb)
 
 (chunk-type task
             goal
+            agent
+            verb
+            object
+            state
             done)
 
 
@@ -40,16 +74,17 @@
   
 (add-dm (nun) (chase) (robber) (active) (passive) (yes)
         (no) (speech-production) (sentence-comprehension)
-        (english)
+        (english) (drawing)
         (sentence1 isa sentence
                    string "the nun chases the robber"
-                   agent nun
+                   noun1 nun
                    verb chase
-                   object robber
+                   noun2 robber
                    voice active
                    syntax-correct yes
                    semantics-correct yes)
         (picture1 isa picture
+                  kind drawing
                   agent nun
                   object robber
                   verb chase)
@@ -66,16 +101,48 @@
                      done no)
         )
 
-;; Comprehension
+;; Sentence Comprehension and Verification
 
-(p parse-sentence
+(p start-verification
    =goal>
      isa task
      goal sentence-comprehension
-==>
+
+   =visual>
+     isa sentence
+
+   ?imaginal>
+     state free
+     buffer empty
+
+ ==>
 )
 
 ;; Production
+
+(p interpret-picture
+   "Transforms a picture into a semantic representation"
+   =goal>
+     isa task
+     goal speech-production
+     done no
+     agent nil
+
+   =visual>
+     isa picture
+     agent =AGENT
+     object =OBJECT
+     verb =VERB
+
+   ?goal>
+     state free
+
+==>
+   *goal>
+     agent =AGENT
+     verb =VERB
+     object =OBJECT 
+)
 
 (p start-speech-production
    "Prepares imaginal buffer and loads agent, oject, and verb in WM"
@@ -83,25 +150,16 @@
      isa task
      goal speech-production
      done no
-
-   =visual>
-     isa picture
-     agent =AGENT
-     object =OBJECT
      verb =VERB
    
    ?imaginal>
      state free
      buffer empty
  ==>
-   =visual>  
+
    +imaginal>
      isa sentence
-     agent =AGENT
-     object =OBJECT
      verb =VERB
-     syntax-correct yes
-     semantics-correct yes
 )
      
 (p retrieve-syntactic-structure
@@ -111,19 +169,9 @@
      goal speech-production
      done no
 
-   =visual>
-     isa picture
-     agent =AGENT
-     object =OBJECT
-     verb =VERB
-
    =imaginal>
      isa sentence
-     agent =AGENT
-     object =OBJECT
      verb =VERB
-     syntax-correct yes
-     semantics-correct yes
      voice nil
 
    ?imaginal>
@@ -133,24 +181,30 @@
      state free
      buffer empty
 ==>
-   =goal>
    =imaginal>
-   =visual>
+
    +retrieval>
      isa syntactic-structure
      language english
-   - voice nil
 )
 
-(p produce-sentence
-   "After retrieving a syntactic structures, you apply it"
+(p apply-syntax-active
+   "If 'active' structure is retrieved, applies the S-V-O structure"
+
+   =goal>
+     isa task
+     goal speech-production
+     agent =AGENT
+     object =OBJECT
+     done no
+   
    ?retrieval>
      state free
      buffer full
 
    =retrieval>
      isa syntactic-structure
-     voice =VOICE
+     voice active
 
    =imaginal>
      isa sentence
@@ -161,18 +215,50 @@
 ==>  
    *imaginal>
      isa sentence
-     voice =VOICE
+     noun1 =AGENT
+     noun2 =OBJECT
+     voice active
 )
+
+
+(p apply-syntax-passive
+   "If 'passive' structure is retrieved, applies the O-V-S structure"
+   =goal>
+     isa task
+     goal speech-production
+     agent =AGENT
+     object =OBJECT
+     done no
+   
+   ?retrieval>
+     state free
+     buffer full
+
+   =retrieval>
+     isa syntactic-structure
+     voice passive
+
+   =imaginal>
+     isa sentence
+     voice nil
+
+   ?imaginal>
+     state free  
+==>  
+   *imaginal>
+     isa sentence
+     noun1 =OBJECT
+     noun2 =AGENT
+     voice active
+)
+
 
 (p done-production
    "After applying it, you are done"
    =imaginal>
      isa sentence
-     agent =AGENT
-     object =OBJECT
-     verb =VERB
-     syntax-correct yes
-     semantics-correct yes
+     noun1 =X
+     noun2 =Y
    - voice nil
      
    =goal>
@@ -182,7 +268,10 @@
      done yes
 )
 
+;;; Produce a sentence
+
+
 (goal-focus speech-goal)
-(set-buffer-chunk 'visual 'sentence1)
+(set-buffer-chunk 'visual 'picture1)
 
 )
