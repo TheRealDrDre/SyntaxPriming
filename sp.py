d957c845159ce728393f8665261b675898e89355
TheRealDrDre <stocco@uw.edu>
2019-04-24 13:39:52 -0700

    New branch for Andrea

---
+++ sp.py
@@ -0,0 +1,8 @@
+# SP Device
+
+# General structure:
+# 1. Present a sentence to visual buffer
+# 2. Run model until it stops
+# 3. Present a picture to visual buffer
+# 4. Run model until it stops
+# 5. Record structure used.
