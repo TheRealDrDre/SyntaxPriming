2af8f40187ed9703cf537fb1a90694e6f60f2f35
TheRealDrDre <stocco@uw.edu>
2019-04-25 23:28:34 -0700

    Working interface and simulation code

--- sp.py
+++ sp.py
@@ -1,9 +1,18 @@
-# SP Device
+## ---------------------------------------------------------------- ##
+## SYNTACTIC PRIMING MODEL MANAGEMENT AND INTERFACE
+## ---------------------------------------------------------------- ##
 
 import actr
 import os
+import random
 
-class Sentence():
+CONDITIONS =  ['AC', 'AI', 'PC', 'PI']
+
+class SP_Object():
+    CONDITIONS = ('AC', 'AI', 'PC', 'PI')
+    pass
+
+class Sentence(SP_Object):
     """A SP experiment stimulus"""
     def __init__(self, condition=None, verb=None, sentence=None):
         if condition is not None:
@@ -46,32 +55,19 @@ class Sentence():
 
     def __repr__(self):
         return self.__str__()
-            
-def import_sentences(fle):
-    """Parses a log and extracts data"""
-    f = open(fle)
-    lines = f.readlines()[1:]
-    N = len(lines)
-    tokens = [x.split(",") for x in lines]
-    selected = [[x[5], x[4]] for x in tokens]
-    sentences = [x[7] for x in tokens]
-    tokenized = [sentence.split() for sentence in sentences]
-    nouns = [[tokens[1], tokens[-1]] for tokens in tokenized]
-    final = [selected[j] + nouns[j] + [sentences[j]] for j in range(N)]
-    # print(final)
-    stimuli = [Sentence(selected[j][0], selected[j][1], sentences[j])
-               for j in range(N)]
-    return stimuli
 
 
-class Picture():
+class Picture(SP_Object):
     """A structure to hold a picture"""
     def __init__(self, agent="drbrown",
                  action="yell",
-                 patient="martymcfly"):
+                 patient="martymcfly",
+                 id = None):
+        """Initializes a picture"""
         self.agent = agent
         self.patient = patient
         self.action = action
+        self.id = id
 
     
     @property
@@ -86,64 +82,158 @@ class Picture():
     
     def __repr__(self):
         """Visual representation"""
-        return "<{P} %s, %s, %s>" % (self.agent,
-                                     self.action,
-                                     self.patient)
+        return "<{%s} %s, %s, %s>" % (self.id,
+                                      self.agent,
+                                      self.action,
+                                      self.patient)
 
     def __str__(self):
         return self.__repr__()
 
+
+class Trial(SP_Object):
+    """Trial"""
+    def __init__(self, condition, sentence, picture):
+        self.condition = condition
+        self.sentence = sentence
+        self.picture = picture
+
+    @property
+    def condition(self):
+        """Returns the condition"""
+        return self._condition
+
+    @condition.setter
+    def condition(self, value):
+        """Sets the condition (Active/Passive, Correct/Incorrect)"""
+        if value.upper() in ['AC', 'AI', 'PC', 'PI']:
+            self._condition = value
+
+        voice = 'active'
+        syntax_correct = 'yes'
     
-def import_pictures(fle):
-    """Imports the pictures used in the study"""
-    return [Picture() for j in range(36)]
+        if self.condition.startswith('P'):
+            voice = 'passive'
 
+        if self.condition.endswith('I'):
+            syntax_correct = 'no'
 
-def run_trial(sentence, picture):
+        self.voice = voice
+        self.syntax_correct = 'no'
+        
+
+    def __str__(self):
+        """A representation of the trial"""
+        return "<[%s] S:%s, P:%s>" % (self.condition,
+                                      self.sentence,
+                                      self.picture)
+
+    def __repr__(self):
+        """A representation of the trial"""
+        return self.__str__()
+
+    
+def load_trials(file="stimuli.txt"):
     """A trial"""
-    chunk_s = actr.define_chunks(sentence.chunk_definition)[0]
-    actr.set_buffer_chunk('visual',
-                          chunk_s)
-    actr.run(time = 2)
+    f = open(file)
+    lines = f.readlines()[1:]
+    N = len(lines)
+    tokenized = [x.split("\t") for x in lines]
+    trials = []
+    for tokens in tokenized:
+        trial_type = tokens[3]
 
-    print("-" * 10)
-    chunk_p = actr.define_chunks(picture.chunk_definition)[0]
-    actr.schedule_set_buffer_chunk('visual',
-                                   chunk_p,
-                                   actr.mp_time() + 0.05)
-    actr.run(time = 5)
+        t_verb = tokens[0]
+        t_image_ID = tokens[2]
+        t_image_agent = tokens[12]
+        t_image_object = tokens[13]
 
-count = 0
+        pic = Picture(agent = t_image_agent,
+                      patient = t_image_object,
+                      action = t_verb,
+                      id = t_image_ID)
 
-def record_response(model, response):
-    """Records a response in the simulations"""
-    global count
-    print("Heyyy '%s'" % response)
-    count += 1
-
-def simulate(model="response-monkey.lisp"):
-    """Simulates stuff"""
-    actr.load_act_r_model(model)
-
-
-    for j in range(10):
-        actr.reset()
-
-        win = actr.open_exp_window("*?*", width = 80,
-                                   height = 60, visible=False)
-        actr.install_device(win)
-        actr.add_command("damn", record_response,
-                         "Accepts a response for the SP task")
-        actr.monitor_command("output-speech",
-                             "damn")
+        p_noun1 = tokens[10]
+        p_noun2 = tokens[11]
+        p_sentence = tokens[4]
         
-        s1 = import_sentences("test.csv")[0]
-        p1 = import_pictures("test.csv")[0]
+        sen = Sentence(condition = trial_type,
+                       verb = t_verb,
+                       sentence = p_sentence)
+
+        trl = Trial(condition = trial_type,
+                    sentence = sen,
+                    picture = pic)
+
+        trials.append(trl)
+
+    return trials
+
+
+class Simulation(SP_Object):
+    def __init__(self, model="procedural2.lisp", n=100):
+        self.model = model
+        self.n = n
+        self.data = {c : 0 for c in self.CONDITIONS}
+        self.trials = load_trials()
+        self.current_condition = None
+
+
+    def record_response(self, model, response):
+        """Records a response in the simulations"""
+        if response == 'active':
+            self.data[self.current_condition] += 1
+        print("Heyyy '%s'" % response)
+
+
+    def run_trial(self, trial):
+        """A trial"""
+        sen = trial.sentence
+        pic = trial.picture
+        chunk_s = actr.define_chunks(sen.chunk_definition)[0]
+        actr.set_buffer_chunk('visual',
+                              chunk_s)
+        actr.run(time = 10)
         
-        run_trial(s1, p1)
+        chunk_p = actr.define_chunks(pic.chunk_definition)[0]
+        actr.schedule_set_buffer_chunk('visual',
+                                       chunk_p,
+                                       actr.mp_time() + 0.05)
+        actr.run(time = 10)
 
 
-        actr.remove_command_monitor("output-speech",
-                                    "damn")
-        actr.remove_command("damn")
+    def simulate(self):
+        actr.load_act_r_model(self.model)
+
+        for condition in self.CONDITIONS:
+            self.current_condition = condition
+            subset = [t for t in self.trials if t.condition == condition]
+            for j in range(self.n):
+                actr.reset()
+        
+                # The model does not really need a visual interface,
+                # but the default AGI provides a virtual mic to record
+                # voice output.
+        
+                win = actr.open_exp_window("SP", width = 80,
+                                           height = 60,
+                                           visible=False)
+                actr.install_device(win)
+                actr.add_command("record-response", self.record_response,
+                                 "Accepts a response for the SP task")
+                actr.monitor_command("output-speech",
+                                     "record-response")
+                
+                trial = random.choice(subset)
+                
+                run_trial(trial)
+                
+                actr.remove_command_monitor("output-speech",
+                                            "record-response")
+                actr.remove_command("record-response")
+
+    def __repr__(self):
+        return "<SMLTN (%s), N=%d>" % (self.model, self.n)
+        
+    
 
